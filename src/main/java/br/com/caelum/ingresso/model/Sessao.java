0b160da0a8ddf9004ed31c7dd1c5c0247e469577
VocêNãoSabeQuemÉ <juninho_lima2020@hotmail.com>
2019-10-15 13:46:07 -0300

    Criando a exibição e listagem das Sessosões :S

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,13 +2,32 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ManyToAny;
+
+@Entity
 public class Sessao {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
+
+	@ManyToOne
 	private Sala sala;
+
+	@ManyToOne
 	private Filme filme;
 
+	@Deprecated
+	public Sessao() {}
+	
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 
 		this.horario = horario;
